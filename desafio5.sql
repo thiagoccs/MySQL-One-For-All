204793007996359f8f4160d01d4f2a46038987fd
Thiago Souza <thiago29.06@hotmail.com>
2022-12-02 15:18:58 -0300

    5° requisito

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+SELECT m.musica AS 'cancao',
+COUNT(id_usuario) AS 'reproducoes'
+FROM SpotifyClone.musicas AS m
+INNER JOIN SpotifyClone.historico AS h
+ON m.id_musica = h.id_musica
+GROUP BY h.id_musica
+ORDER BY reproducoes DESC, cancao ASC
+LIMIT 2
