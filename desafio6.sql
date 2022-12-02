95be75095da34d0308c6cadd6d7c0381bf2cd6c0
Thiago Souza <thiago29.06@hotmail.com>
2022-12-02 16:17:20 -0300

    6° requisito arrumado para 0.00

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT MIN(p.valor) AS 'faturamento_minimo',
+SELECT FORMAT(MIN(p.valor),2) AS 'faturamento_minimo',
 MAX(p.valor) AS 'faturamento_maximo',
 ROUND(AVG(p.valor),2) AS 'faturamento_medio',
 ROUND(SUM(p.valor),2) AS 'faturamento_total'
