73dd5cf9e24d1d8626fc451ddf6df957dbe9b48d
Thiago Souza <thiago29.06@hotmail.com>
2022-12-03 11:45:38 -0300

    7° requisito

--- desafio7.sql
+++ desafio7.sql
@@ -7,4 +7,5 @@ ON art.id_artista = a.id_artista
 INNER JOIN SpotifyClone.seguindo_artistas AS s
 ON art.id_artista = s.id_artista
 GROUP BY art.artista, a.album
-ORDER BY seguidores DESC, artista ASC, album ASC;
+ORDER BY seguidores DESC, artista ASC, album ASC
+;
